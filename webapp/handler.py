e73b400e2e256a0c9a23f570d5f33ffa035b1846
Rangel Varnier <rangel.varnier@gmail.com>
2021-03-11 06:27:21 -0300

    webapp

--- webapp/handler.py
+++ webapp/handler.py
@@ -39,5 +39,5 @@ def rossmann_predict():
 
 
 if __name__ == '__main__':
-    port = os.environ.get('PORT', 5000)
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run( host = '0.0.0.0', port = port)
